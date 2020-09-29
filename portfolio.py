eb000bbf4fc81bd24545f3976ea8b56c951d4346
huiseom.kim <huiseom.kim@hancom.com>
2020-09-29 14:54:36 +0900

    todo

--- portfolio.py
+++ portfolio.py
@@ -7,33 +7,24 @@ from pandas_datareader import data as pdr
 
 class Portfoilo():
     def __init__(self):
-        self.stock_codes=[]
-        self.df_stock = pd.DataFrame()
-    
-    def load_price_data(self, start_date, end_date):
-        tmp =[]
-        for stock_code in self.stock_codes:
-            if 'KS' in stock_code:
-                tmp = pdr.DataReader(stock_code, 'yahoo', start_date, end_date)
-            elif 'KQ' in stock_code:
-                tmp = fdr.DataReader(stock_code[:-3], start_date, end_date)
-            self.df_stock[stock_code] = tmp['Close']
+        self.stocks = pd.DataFrame(columns=['stock_name','stock_code','VaR', 'Corr'])
 
-    def add_stock(self, stock_codes):
-        self.stock_codes.append(stock_codes)
-        if(np.shape(self.stock_codes)[0] != 1): # flatten
-            self.stock_codes = list(itertools.chain(*self.stock_codes))
-    
-    def corr(self):
-        return self.df_stock.corr()
+    def add_stock_to_pf(self, stock_name, stock_code):
+        self.stocks
+        return
+    def del_stock_from_pf(self, stock_code):
+        return
 
 
-def test():
-    pf = Portfoilo()
-    pf.add_stock(['003670.KS', '069960.KS'])
-    pf.add_stock(['099320.KQ', '030520.KQ'])
-    print(pf.stock_codes)
-    pf.load_price_data(start_date='2019-03-02', end_date='2020-03-21')
-    print(pf.df_stock)
-    print(pf.corr())
-test()
+class Analysis(Portfoilo):
+    def __init__(self):
+        return
+
+    def load_price_data(self, stock_code):
+        return
+
+    def cal_corr(self, pf_name):
+        return
+
+    def cal_var(self, pf_name):
+        return
